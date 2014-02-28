f624885dec7e1d075c3df2bcaec43d0fb05c2bfc
Kevin <dev@sterrehoeve.nl>
2014-02-28 15:36:52 +0100

    Initial Commit

---
+++ src/nl/sterrehoeve/nexus/Nexus.java
@@ -0,0 +1,15 @@
+package nl.sterrehoeve.nexus;
+
+
+/**
+ * Created by Kevin on 28-2-14.
+ */
+public class Nexus {
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+	}
+
+}
